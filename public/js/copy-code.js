85b28f5cb0d86e217c9db27ce8a35ba26c9ee35c
yzget <zhaolexe@gmail.com>
2026-07-03 18:49:54 +0800

    更新了博客文章

--- public/js/copy-code.js
+++
@@ -1,8 +0,0 @@
-function copyCode(btn) {
-  const code = btn.nextElementSibling.innerText;
-
-  navigator.clipboard.writeText(code).then(() => {
-    btn.innerText = "Copied!";
-    setTimeout(() => (btn.innerText = "Copy"), 1500);
-  });
-}
